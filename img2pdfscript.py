a5f0e542becfcbc20bbbeb17e349a058cb64230b
Jochen Jankowai <jochen.jankowai@gmail.com>
2018-08-07 10:30:37 +0200

    Some coloring :)

--- img2pdfscript.py
+++ img2pdfscript.py
@@ -3,11 +3,15 @@ from fpdf import FPDF
 import argparse
 import glob
 from PIL import Image
+from colorama import init, Fore, Back, Style
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path", type=str)
 parser.add_argument("dpi", type=int)
 args = parser.parse_args()
+
+init()
 
 images = glob.glob(args.path + '/**/*.png', recursive=True)
 images += glob.glob(args.path + '/**/*.jpg', recursive=True)
@@ -20,15 +24,15 @@ if num_images == 0:
 
 i = 0
 
+print(Fore.CYAN+ "__________________________________________________\n")
+print(Style.BRIGHT + "Converting " + str(num_images) + " images to PDFs at a quality of " + str(args.dpi) + "DPI." + Style.NORMAL)
 print("__________________________________________________\n")
-print("Converting " + str(num_images) + " images to PDFs at a quality of " + str(args.dpi) + "DPI.")
-print("__________________________________________________")
 
 for image in images:
     absolute_path = os.path.dirname(os.path.abspath(image))
     pdf_filename = absolute_path + '/' + os.path.splitext(os.path.basename(image))[0] + ".pdf"
 
-    print("Generating PDF file (" + str(i + 1) + "/" + str(num_images) + "): " + pdf_filename)
+    print("Generating PDF file (" + Fore.MAGENTA + Style.BRIGHT + str(i + 1) + Style.NORMAL + Fore.CYAN + "/" + Fore.MAGENTA + str(num_images) + Fore.CYAN + "): " + pdf_filename)
 
     with Image.open(image) as img:
         width_px, height_px = img.size
