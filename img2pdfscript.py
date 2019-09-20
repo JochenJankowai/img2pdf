e99626145cd346d83d01e4b04b608090040940c6
Jochen Jankowai <jochen.jankowai@liu.se>
2019-09-20 09:41:40 +0200

    Update

--- img2pdfscript.py
+++ img2pdfscript.py
@@ -24,18 +24,22 @@ if num_images == 0:
 
 i = 0
 
-print(Fore.CYAN+ "__________________________________________________\n")
+print(Fore.CYAN + "____________________________________________________\n")
 print(Style.BRIGHT + "Converting " + str(num_images) + " images to PDFs at a quality of " + str(args.dpi) + "DPI." + Style.NORMAL)
-print("__________________________________________________\n")
+print("____________________________________________________\n")
 
 for image in images:
     absolute_path = os.path.dirname(os.path.abspath(image))
-    pdf_filename = absolute_path + '/' + os.path.splitext(os.path.basename(image))[0] + ".pdf"
-    ps_filename = absolute_path + '/' + os.path.splitext(os.path.basename(image))[0] + ".ps"
+    folder_name = os.path.split(absolute_path)[1]
+    pdf_filename = os.path.splitext(os.path.basename(image))[0] + ".pdf"
+    ps_filename = os.path.splitext(os.path.basename(image))[0] + ".ps"
+    absolute_pdf_filename = absolute_path + '/' + pdf_filename
+    absolute_ps_filename = absolute_path + '/' + ps_filename
 
     space = ((i+1) / 10) < 0.98
 
-    print("Generating PDF file (" + (" " if space else "") + Fore.MAGENTA + Style.BRIGHT + str(i + 1) + Style.NORMAL + Fore.CYAN + "/" + Fore.MAGENTA + str(num_images) + Fore.CYAN + "): " + pdf_filename)
+    print("Generating PDF file (" + (" " if space else "") + Fore.MAGENTA + Style.BRIGHT + str(i + 1) + Style.NORMAL
+          + Fore.CYAN + "/" + Fore.MAGENTA + str(num_images) + Fore.CYAN + "): " + folder_name + "/" + pdf_filename)
 
     with Image.open(image) as img:
         width_px, height_px = img.size
@@ -46,14 +50,16 @@ for image in images:
         pdf.add_page()
         pdf.image(image, 0, 0, width_in, height_in)
 
-        pdf.output(pdf_filename, "F")
+        pdf.output(absolute_pdf_filename, "F")
 
     i = i + 1
 
     # We need to convert the PDF to PS and back because of some meta data that is added by FPDF which Latex doesn't like
-    subprocess.check_output(['pdf2ps', pdf_filename, ps_filename])
+    subprocess.check_output(['pdf2ps', absolute_pdf_filename, absolute_ps_filename])
 
-    subprocess.check_output(['ps2pdf', ps_filename, pdf_filename])
+    subprocess.check_output(['ps2pdf', absolute_ps_filename, absolute_pdf_filename])
+
+    subprocess.check_output(['rm', absolute_ps_filename ])
 
 
 print(Style.RESET_ALL)
