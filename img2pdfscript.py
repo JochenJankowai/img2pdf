349a5c70e597db36d09585f992a315f852768b7a
Jochen Jankowai <jochen.jankowai@liu.se>
2022-03-10 08:34:36 +0100

    Check if file already exists

--- img2pdfscript.py
+++ img2pdfscript.py
@@ -25,7 +25,8 @@ if num_images == 0:
 i = 0
 
 print(Fore.CYAN + "____________________________________________________\n")
-print(Style.BRIGHT + "Converting " + str(num_images) + " images to PDFs at a quality of " + str(args.dpi) + "DPI." + Style.NORMAL)
+print(Style.BRIGHT + "Converting " + str(num_images) +
+      " images to PDFs at a quality of " + str(args.dpi) + "DPI." + Style.NORMAL)
 print("____________________________________________________\n")
 
 for image in images:
@@ -33,33 +34,49 @@ for image in images:
     folder_name = os.path.split(absolute_path)[1]
     pdf_filename = os.path.splitext(os.path.basename(image))[0] + ".pdf"
     ps_filename = os.path.splitext(os.path.basename(image))[0] + ".ps"
-    absolute_pdf_filename = os.path.sep.join([absolute_path,pdf_filename])
-    absolute_ps_filename = os.path.sep.join([absolute_path,ps_filename])
+    absolute_pdf_filename = os.path.sep.join([absolute_path, pdf_filename])
+    absolute_ps_filename = os.path.sep.join([absolute_path, ps_filename])
 
     space = ((i+1) / 10) < 0.98
+
+    if os.path.exists(absolute_pdf_filename):
+        print(Fore.YELLOW + "Already converted, skipping.")
+        i = i + 1
+        continue
 
     print("Generating PDF file (" + (" " if space else "") + Fore.MAGENTA + Style.BRIGHT + str(i + 1) + Style.NORMAL
           + Fore.CYAN + "/" + Fore.MAGENTA + str(num_images) + Fore.CYAN + "): " + folder_name + "/" + pdf_filename)
 
-    with Image.open(image) as img:
-        width_px, height_px = img.size
-        width_in = width_px/args.dpi
-        height_in = height_px/args.dpi
+    try:
+        with Image.open(str(image), "r") as img:
+            width_px, height_px = img.size
+            width_in = width_px/args.dpi
+            height_in = height_px/args.dpi
 
-        pdf = FPDF(unit='in', format=tuple((width_in,height_in)))
-        pdf.add_page()
-        pdf.image(image, 0, 0, width_in, height_in)
+            pdf = FPDF(unit='in', format=tuple((width_in, height_in)))
+            pdf.add_page()
+            pdf.image(image, 0, 0, width_in, height_in)
 
-        pdf.output(absolute_pdf_filename, "F")
+            pdf.output(absolute_pdf_filename, "F")
 
-    i = i + 1
+        # We need to convert the PDF to PS and back because of some meta data that is added by FPDF which Latex doesn't like
+        subprocess.check_output(
+            ['pdf2ps', absolute_pdf_filename, absolute_ps_filename])
 
-    # We need to convert the PDF to PS and back because of some meta data that is added by FPDF which Latex doesn't like
-    subprocess.check_output(['pdf2ps', absolute_pdf_filename, absolute_ps_filename])
+        subprocess.check_output(
+            ['ps2pdf', absolute_ps_filename, absolute_pdf_filename])
 
-    subprocess.check_output(['ps2pdf', absolute_ps_filename, absolute_pdf_filename])
-
-    subprocess.check_output(['rm', absolute_ps_filename ])
+        subprocess.check_output(['rm', absolute_ps_filename])
+    except RuntimeError as e:
+        print(Fore.RED)
+        print(e)
+        print(Fore.CYAN)
+    except FileNotFoundError as e:
+        print(Fore.YELLOW + Style.NORMAL)
+        print("Warn")
+        print(Fore.CYAN)
+    finally:
+        i = i + 1
 
 
 print(Style.RESET_ALL)
